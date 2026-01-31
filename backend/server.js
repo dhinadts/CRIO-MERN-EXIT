d81a54d36d7cc187033c839c5cb4fea4779b7a1c
dhinadts <dhinadts@gmail.com>
2026-01-31 16:35:24 +0530

    Backend api Map in cloud

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: "http://localhost:3000", // allow React dev server 
+        origin: "https://crio-mern-xexit.onrender.com", // allow React dev server 
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
